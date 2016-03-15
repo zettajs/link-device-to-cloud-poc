072225433e71c24b0d0ab42d62fad13de739ed57
Adam Magaluk <AdamMagaluK@gmail.com>
2016-03-15 14:53:46 -0600

    Added etcd files

---
+++ start-devices.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ "$1" != "" ]]; then
+    CLIENTS="$1"
+else
+    CLIENTS=1
+fi
+
+startChild() {
+    node example_device_subscriptions.js $1 $2 &
+}
+
+COUNTER=0
+while [  $COUNTER -lt $CLIENTS ]; do
+    
+    data=$(curl -s -H "Content-Type:application/json" --data '{"name": "somedevice" }' http://localhost:1338/)
+    username=$(echo $data | jq -r '.properties.username')
+    password=$(echo $data | jq -r '.properties.password')
+    startChild $username $password
+    let COUNTER=COUNTER+1
+done
+
+wait
